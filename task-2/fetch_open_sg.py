5a68aadd20b9a6bf5fe5592d00b645f95eaf2b2a
Sarvesh Anand <sarvesh.anand@gwynniebee.com>
2019-08-05 15:47:57 +0530

    added task2

---
+++ task-2/fetch_open_sg.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+# Variables to be supplied
+# 1.Region 2.Mail credentials for smtp client
+
+# Importing python modules
+import boto3
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+# Declaring variables
+open_sgs = {"SG" : {}}
+grp_data = []
+region = "eu-central-1"
+ec2=boto3.client('ec2', region )
+security_groups = ec2.describe_security_groups()
+security_groups = security_groups["SecurityGroups"]
+recipients = ['sarveshanand94@gmail.com']
+
+# Func to sendmail
+def send_email(email_body,recipients,subject):
+  msg = MIMEMultipart()
+  msg['From'] = 'sarveshanand94@gmail.com'
+  msg['To'] = (',').join(recipients)
+  msg['Subject'] = subject
+  msg.attach(MIMEText(email_body, 'plain'))
+  password = 'XXXXXX'
+  email = smtplib.SMTP('smtp.gmail.com:587')
+  email.starttls()
+  email.login(msg['From'], password)
+  email.sendmail(msg['From'], recipients, msg.as_string())
+  email.quit()
+
+# Program starts here
+for security_group in security_groups:
+    security_ip_permissions = security_group['IpPermissions']
+    security_group_name = security_group['GroupName']
+    security_group_id = security_group['GroupId']
+    for per_rule in security_ip_permissions:
+        for item in per_rule['IpRanges']:
+            if 'CidrIp' in item.keys():
+                if item['CidrIp'] == '0.0.0.0/0':
+                    open_sgs['SG'][security_group_name] = security_group_id
+
+
+for key,value in open_sgs['SG'].iteritems():
+    tmp = '%s : %s' %(key,value)
+    grp_data.append(tmp)
+
+print grp_data
+msg = "Below Security groups are found to be exposed to outside world\n\n%s" % ("\n".join(grp_data))
+send_email(msg, recipients,"EXPOSED SECURITY GROUPS IN REGION %s" %(region))
